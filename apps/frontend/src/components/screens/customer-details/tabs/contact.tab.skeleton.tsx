a21c06300e7e00c394ef82956a09b5d4e2ed5d94
MikaelMarceniuk <mika.marceniuk@gmail.com>
2025-11-13 18:20:12 +0000

    fix contactTabSkeleton

--- apps/frontend/src/components/screens/customer-details/tabs/contact.tab.skeleton.tsx
+++ apps/frontend/src/components/screens/customer-details/tabs/contact.tab.skeleton.tsx
@@ -5,7 +5,7 @@ export const ContactTabSkeleton = () => {
     <div className="bg-muted/30 rounded-md p-4">
       <h2 className="mb-4 text-lg font-semibold">Contato</h2>
 
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4">
         <div className="flex flex-col space-y-2">
           <Skeleton className="h-4 w-24" /> {/* label */}
           <Skeleton className="h-10 w-full" /> {/* input */}
